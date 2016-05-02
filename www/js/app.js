9fdbaa5cf48919ef3192d70a78297b8dc755087a
halokin <halokin@gmail.com>
2016-05-02 22:07:59 +0200

    routing

--- www/js/app.js
+++ www/js/app.js
@@ -23,6 +23,26 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         });
     })
 
+.config(function($stateProvider, $urlRouterProvider){
+    $stateProvider
+    .state('tabs', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+    })
+    .state('tabs.list', {
+        url: '/list',
+        views: {
+            'list-tab' : {
+                templateUrl: 'templates/list.html', 
+                controller: 'ListController'
+            }
+        }
+    })
+    $urlRouterProvider.otherwise('/tab/list');
+})
+
+
 .controller('ListController', ['$scope', '$http', function ($scope, $http)
         {
             $http.get('js/data.json').success(function (data) {
