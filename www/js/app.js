6163fa4f6fa8b827df76b5b4779febfd596f5b71
halokin <halokin@gmail.com>
2016-05-02 17:07:26 +0200

    splice et move list

--- www/js/app.js
+++ www/js/app.js
@@ -27,6 +27,11 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         {
             $http.get('js/data.json').success(function (data) {
                 $scope.artists = data;
+                $scope.moveItem = function(item, fromIndex, toIndex){
+                    $scope.artists.splice(fromIndex, 1);
+                    $scope.artists.splice(toIndex, 0, item);
+                    
+                };
 
             });
 }]);
